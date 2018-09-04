3c0ec7a54bbd86cc7007260834002f1c2a846e36
John Rapp <johnrapp814@gmail.com>
2018-09-03 21:16:08 -0400

    master - fixing issue where navbar would disappear on page refresh

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var lastScroll = 0;
 
 window.onscroll = () => {
   var currentY = window.pageYOffset;
-  if (lastScroll > currentY) {
+  if (lastScroll > currentY || lastScroll === 0) {
     navbar.style.top = '0';
   } else {
     navbar.style.top = '-100px';
